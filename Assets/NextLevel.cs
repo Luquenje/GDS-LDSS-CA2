afe0bb030892832aa30831468dc59a16812f47ad
sinhaugoh <sinhaugoh.16@ichat.sp.edu.sg>
2018-02-14 19:21:46 +0800

    lv1 to 2 loading done

--- Assets/NextLevel.cs
+++ Assets/NextLevel.cs
@@ -2,11 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class NextLevel : MonoBehaviour {
 
     //SceneManage scene;
-    public string sceneName = "CaveLevel";
+    //public string sceneName = "CaveLevel";
+    //public int sceneIndex;
+    public GameObject finishScene;
+    public GameObject loadingScreen;
+    public Slider slider;
 	// Use this for initialization
 	void Start () {
 
@@ -20,7 +25,26 @@ public class NextLevel : MonoBehaviour {
     {
         if (other.gameObject.tag == "Player")
         {
-            SceneManager.LoadScene(sceneName);
+            finishScene.SetActive(true);
+        }
+        Cursor.visible = true;
+    }
+    public void LoadLv(int sceneIndex)
+    {
+        StartCoroutine(LoadAsynchronously(sceneIndex));
+    }
+
+    IEnumerator LoadAsynchronously(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        loadingScreen.SetActive(true);
+        finishScene.SetActive(false);
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            slider.value = progress;
+            Debug.Log(progress);
+            yield return null;
         }
     }
 }
